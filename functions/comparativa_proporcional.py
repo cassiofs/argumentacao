e931a3c255efb72c552ed8a336e260344dc74e9c
cassiofs <cassiofs@gmail.com>
2022-03-04 11:34:51 -0300

    Update comparativa_proporcional.py

--- functions/comparativa_proporcional.py
+++ functions/comparativa_proporcional.py
@@ -1,3 +1,4 @@
+# Comparativa ou Proporcional (ex: assim como, da mesma forma que, mal dá … quem dirá, nem um… nem outro, quanto mais… melhor, quem dirá)
 import re
 text = "veremos que, assim como, o outro também"
 hashtags = re.findall(r'assim como|da mesma forma|mal dá|quem dirá|nem um|nem outro|quanto mais|quem dirá', text)
